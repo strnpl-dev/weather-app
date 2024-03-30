4f7d73580c7eba1ee0c8e3e1fd21063ad31551b2
Robert <strnpl.dev@gmail.com>
2024-03-30 09:11:44 +0300

    added js

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
     let api = '81cd96a4cf8f4303b5e111110242903'
 
+    const locationBtn = document.querySelector('weather__btns-wrapper'),
+        languageChange = document.querySelector('#languageChange'),
+        city = document.querySelector('.weather__city'),
+        date = document.querySelector('.weather__time-date'),
+        time = document.querySelector('.weather__time-time'),
+        weatherNum = document.querySelector('weather__conditions-num'),
+        futureNums = document.querySelectorAll('.weather__item-num'),
+        locationInput = document.querySelector('#weather_form input'),
+        searchBtn = document.querySelector('.weather__control-btn'),
+        latitude = document.querySelector('.latitude'),
+        longitude = document.querySelector('longitude')
+
+
+
 })
